9ed5d52fe2b1e2238ec4a47679b7f782a7b7495d
d.efimov <d.efimov@fun-box.ru>
2019-09-28 15:47:54 +0300

    use cors

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,15 @@ Bundler.require(*Rails.groups)
 
 module Arback
   class Application < Rails::Application
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+          :headers => :any,
+          :expose => ['access-token', 'expiry', 'token-type', 'Authorization'],
+          :methods => [:get, :post, :options, :delete, :put]
+      end
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
