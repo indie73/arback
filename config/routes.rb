9e26d01ed0dc8bb6702cba3b9f46f56b41ef24ad
d.efimov <d.efimov@fun-box.ru>
2019-09-28 23:32:34 +0300

    Упростить API

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: 'json'} do
     namespace :v1 do
-      resources :files, only: %i[create index show]
       resources :instructions, only: %i[index show]
       resources :control_points, only: %i[index]
     end
