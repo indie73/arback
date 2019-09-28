c9dbfa70f4a4b9ef8157cc5c2d5408f5ff7c83e8
d.efimov <d.efimov@fun-box.ru>
2019-09-28 20:55:50 +0300

    Добавить аннотации для моделей

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: details
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  short_name :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Detail < ApplicationRecord
 end
--- spec/models/detail_spec.rb
+++ spec/models/detail_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: details
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  short_name :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Detail, type: :model do
